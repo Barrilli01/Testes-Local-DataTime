f6792098e83087592df21b252266ac54e203d30c
Barrilli01 <gabrielbarrillicruz@gmail.com>
2024-01-10 10:39:40 -0300

    Mudança do horário de saída

--- src/EspacoDeTempo.java
+++ src/EspacoDeTempo.java
@@ -4,19 +4,21 @@ import java.time.LocalTime;
 
 public class EspacoDeTempo {
     public void calculaTempo(){
+
         LocalDate dataEntrada = LocalDate.now();
         LocalTime horaEntrada = LocalTime.now();
 
-        LocalDate dataSaida = LocalDate.of(2024 , 2 , 10);
-        LocalTime horaSaida = LocalTime.of(9 , 15);
+        LocalDate dataSaida = LocalDate.of(2024 , 1 , 10);
+        LocalTime horaSaida = LocalTime.of(20 , 30);
 
         Duration duracao = Duration.between(dataEntrada.atTime(horaEntrada), dataSaida.atTime(horaSaida));
 
         // Obtém a diferença em horas e minutos
         long horas = duracao.toHours();
         long minutos = duracao.toMinutes() % 60;
+        long segundos = duracao.toSeconds() % 60;
 
         // Exibe o tempo total de permanência
-        System.out.println("Tempo total de permanência: " + horas + " horas e " + minutos + " minutos.");
+        System.out.println("Tempo total de permanência: " + horas + " horas " + minutos + " minutos e " + segundos + " segundos");
     }
 }
